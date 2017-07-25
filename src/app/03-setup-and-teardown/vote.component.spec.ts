463a689835a23c404274f4f986ef9fb9f388d6c7
r.sherman@jayex.com <r.sherman@jayex.com>
2017-07-25 09:19:03 +0100

    Complete lesson

--- src/app/03-setup-and-teardown/vote.component.spec.ts
+++ src/app/03-setup-and-teardown/vote.component.spec.ts
@@ -1,9 +1,18 @@
 import { VoteComponent } from './vote.component'; 
 
 describe('VoteComponent', () => {
-  it('', () => {
+  let component: VoteComponent;
+  beforeEach(() => {
+    component = new VoteComponent();
   });
 
-  it('', () => {
+  it('should increment total votes when upvoted', () => {
+    component.upVote();
+    expect(component.totalVotes).toBe(1);
+  });
+
+  it('should decrease total votes when downvoted', () => {
+    component.downVote();
+    expect(component.totalVotes).toBe(-1);
   });
 });
